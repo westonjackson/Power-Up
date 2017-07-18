ecae74342a30656aae66ba172629a9d1e8b00528
westonjackson <weston.j.jackson@gmail.com>
2017-07-18 13:48:02 -0400

    fixed routing

--- app.py
+++ app.py
@@ -5,12 +5,31 @@ import logging
 from logging import Formatter, FileHandler
 import os
 
-
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template('home.html')
+
+@app.route('/heatmap')
+def heatmap():
+    return render_template('heatmap.html')
+
+@app.route('/users')
+def users():
+    return render_template('users.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/livefeed')
+def livefeed():
+    return render_template('livefeed.html')
+
+@app.route('/alertbutton')
+def alertbutton():
+    return render_template('alertbutton.html')
 
 if __name__ == '__main__':
     app.run()
