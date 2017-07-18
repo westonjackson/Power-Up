b8c92d8ae308ccee818472362d5e461394bad358
westonjackson <weston.j.jackson@gmail.com>
2017-07-18 15:06:32 -0400

    database

--- schema.sql
+++ schema.sql
@@ -1,6 +1,7 @@
-drop table if exists alert;
-    create table alert (
+drop table if exists alerts;
+    create table alerts (
             id integer primary key autoincrement,
             name text not null,
+            time text not null,
             location text not null
         );
