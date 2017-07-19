d183709054e258620f3af914e9241d304cc4353d
sberry <sberry@appnexus.com>
2017-07-19 12:44:56 -0400

    updated report / alert schema

--- schema.sql
+++ schema.sql
@@ -3,5 +3,9 @@ drop table if exists alerts;
             id integer primary key autoincrement,
             name text not null,
             time text not null,
-            location text not null
+            location text null,
+            lat integer null,
+            long integer null,
+            description text null
+            anonymous integer not null
         );
